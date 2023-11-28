4f0ff85a73dd57b19cecb55c88fee6b41a88f0f3
Victor Torres <vatorresm88@gmail.com>
2023-11-28 15:42:16 -0500

    Creando el cat DTO

--- src/cats/dto/create-cat.dto.ts
+++ src/cats/dto/create-cat.dto.ts
@@ -1,1 +1,14 @@
-export class CreateCatDto {}
+import { IsInt, IsOptional, IsPositive, IsString } from 'class-validator';
+
+export class CreateCatDto {
+  @IsString()
+  name: string;
+
+  @IsInt()
+  @IsPositive()
+  age: number;
+
+  @IsString()
+  @IsOptional()
+  breed: string;
+}
